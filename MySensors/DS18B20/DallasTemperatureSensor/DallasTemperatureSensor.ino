d5673eee489c3fc248850934c1ac225f45e584bb
Andrii Natalenko <fineftp@gmail.com>
2018-07-24 16:00:14 +0300

    ds18b20+bat

--- MySensors/DS18B20/DallasTemperatureSensor/DallasTemperatureSensor.ino
+++ MySensors/DS18B20/DallasTemperatureSensor/DallasTemperatureSensor.ino
@@ -26,24 +26,27 @@
 
 
 // Enable debug prints to serial monitor
-#define MY_DEBUG 
+#define MY_DEBUG
 
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
-
+#define MY_NODE_ID 10
 #include <SPI.h>
-#include <MySensors.h>  
+#include <MySensors.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
 
+int BATTERY_SENSE_PIN = A0;  // select the input pin for the battery sense point
+
 #define COMPARE_TEMP 1 // Send temperature only if changed? 1 = Yes 0 = No
 
-#define ONE_WIRE_BUS 3 // Pin where dallase sensor is connected 
+#define ONE_WIRE_BUS 3 // Pin where dallase sensor is connected
 #define MAX_ATTACHED_DS18B20 16
-unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
+unsigned long SLEEP_TIME = 3600000; // Sleep time between reads (in milliseconds)
+int oldBatteryPcnt = 0;
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-DallasTemperature sensors(&oneWire); // Pass the oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire); // Pass the oneWire reference to Dallas Temperature.
 float lastTemperature[MAX_ATTACHED_DS18B20];
 int numSensors=0;
 bool receivedConfig = false;
@@ -57,53 +60,70 @@ void before()
   sensors.begin();
 }
 
-void setup()  
-{ 
+void setup()
+{
+  // use the 1.1 V internal reference
+#if defined(__AVR_ATmega2560__)
+  analogReference(INTERNAL1V1);
+#else
+  analogReference(INTERNAL);
+#endif
   // requestTemperatures() will not block current thread
   sensors.setWaitForConversion(false);
 }
 
 void presentation() {
   // Send the sketch version information to the gateway and Controller
-  sendSketchInfo("Temperature Sensor", "1.1");
+  sendSketchInfo("Temperature Sensor + Battery", "1.1");
 
-  // Fetch the number of attached temperature sensors  
+  // Fetch the number of attached temperature sensors
   numSensors = sensors.getDeviceCount();
 
   // Present all sensors to controller
-  for (int i=0; i<numSensors && i<MAX_ATTACHED_DS18B20; i++) {   
+  for (int i=0; i<numSensors && i<MAX_ATTACHED_DS18B20; i++) {
      present(i, S_TEMP);
   }
 }
 
-void loop()     
-{     
+void loop()
+{
   // Fetch temperatures from Dallas sensors
   sensors.requestTemperatures();
-
+  // get the battery Voltage
+  int sensorValue = analogRead(BATTERY_SENSE_PIN);
   // query conversion time and sleep until conversion completed
   int16_t conversionTime = sensors.millisToWaitForConversion(sensors.getResolution());
   // sleep() call can be replaced by wait() call if node need to process incoming messages (or if node is repeater)
   sleep(conversionTime);
+  // 1M, 470K divider across battery and using internal ADC ref of 1.1V
+  // Sense point is bypassed with 0.1 uF cap to reduce noise at that point
+  // ((1e6+470e3)/470e3)*1.1 = Vmax = 3.44 Volts
+  // 3.44/1023 = Volts per bit = 0.003363075
 
-  // Read temperatures and send them to controller 
+  int batteryPcnt = sensorValue / 10;
+  // Read temperatures and send them to controller
   for (int i=0; i<numSensors && i<MAX_ATTACHED_DS18B20; i++) {
- 
+
     // Fetch and round temperature to one decimal
     float temperature = static_cast<float>(static_cast<int>((getControllerConfig().isMetric?sensors.getTempCByIndex(i):sensors.getTempFByIndex(i)) * 10.)) / 10.;
- 
+
     // Only send data if temperature has changed and no error
     #if COMPARE_TEMP == 1
     if (lastTemperature[i] != temperature && temperature != -127.00 && temperature != 85.00) {
     #else
     if (temperature != -127.00 && temperature != 85.00) {
     #endif
- 
+
       // Send in the new temperature
       send(msg.setSensor(i).set(temperature,1));
       // Save new temperatures for next compare
       lastTemperature[i]=temperature;
     }
+  }
+  if (oldBatteryPcnt != batteryPcnt) {
+    // Power up radio after sleep
+    sendBatteryLevel(batteryPcnt);
+    oldBatteryPcnt = batteryPcnt;
   }
   sleep(SLEEP_TIME);
 }
